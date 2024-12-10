a434bc31acfcd2e83dd4e29a97bc9297fde4dbe5
Andi Alifsyah <andyalyfsyah4@gmail.com>
2024-12-10 15:07:38 +0700

    update path

--- FLUENT_REFACTORED_24/data.py
+++ FLUENT_REFACTORED_24/data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 print("start loading data from data.py")
+
+base_path = '/home/andyalyfsyah/FLUENT-Chatbot-2023/FLUENT_REFACTORED_24'
 # knowledgebase_url = 'https://github.com/AndiAlifs/FLUENT-Chatbot-2023/raw/main/KnowledgeBaseFilkom.xlsx'
-knowledgebase_url = 'FLUENT-Chatbot-2023/KnowledgeBaseFilkom.xlsx'
+knowledgebase_url = base_path + 'KnowledgeBaseFilkom.xlsx'
 knowledgebase = pd.read_excel(knowledgebase_url)
-knowledgebase_eval_url = 'FLUENT-Chatbot-2023/KnowledgeBaseFilkom_eval.xlsx'
+knowledgebase_eval_url = base_path + 'FLUENT-Chatbot-2023/KnowledgeBaseFilkom_eval.xlsx'
 knowledgebase_eval = pd.read_excel(knowledgebase_eval_url)
 
 qa_paired = knowledgebase.drop(columns=knowledgebase.columns.drop(['Pertanyaan', 'Jawaban']))
