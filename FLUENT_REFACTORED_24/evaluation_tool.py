2b55e1bb3123fb8f372d8df7881f21158d57a0ee
Andi Alifsyah <andyalyfsyah4@gmail.com>
2024-12-10 17:36:42 +0700

    update main

---
+++ FLUENT_REFACTORED_24/evaluation_tool.py
@@ -0,0 +1,49 @@
+def calculate_bleu(preds, questions, answers):
+    bleu_score_1 = 0
+    bleu_score_2 = 0
+    bleu_score_3 = 0
+    bleu_score_4 = 0
+    cum_bleu_score_1 = 0
+    cum_bleu_score_2 = 0
+    cum_bleu_score_3 = 0
+    cum_bleu_score_4 = 0
+
+    num_of_rows_calculated = 0
+    smoothing_function = SmoothingFunction().method1
+
+    for i, (question, real_answer) in enumerate(zip(questions, answers)):
+        # print(f"Question: {question}")
+        # print(f"Real Answer: {real_answer}")
+        # print(f"Predicted Answer: {preds[i]}")
+        refs = [real_answer.split(' ')]
+        hyp = preds[i].split(' ')
+
+        bleu_score_1 += sentence_bleu(refs, hyp, weights=(1,0,0,0), smoothing_function=smoothing_function)
+        bleu_score_2 += sentence_bleu(refs, hyp, weights=(0,1,0,0), smoothing_function=smoothing_function)
+        bleu_score_3 += sentence_bleu(refs, hyp, weights=(0,0,1,0), smoothing_function=smoothing_function)
+        bleu_score_4 += sentence_bleu(refs, hyp, weights=(0,0,0,1), smoothing_function=smoothing_function)
+        cum_bleu_score_1 += sentence_bleu(refs, hyp, weights=(1,0,0,0), smoothing_function=smoothing_function)
+        cum_bleu_score_2 += sentence_bleu(refs, hyp, weights=(0.5,0.5,0,0), smoothing_function=smoothing_function)
+        cum_bleu_score_3 += sentence_bleu(refs, hyp, weights=(0.33,0.33,0.33,0), smoothing_function=smoothing_function)
+        cum_bleu_score_4 += sentence_bleu(refs, hyp, weights=(0.25,0.25,0.25,0.25), smoothing_function=smoothing_function)
+
+        num_of_rows_calculated+=1
+
+    results = {"1-gram": (bleu_score_1/num_of_rows_calculated),
+                "2-gram": (bleu_score_2/num_of_rows_calculated),
+                "3-gram": (bleu_score_3/num_of_rows_calculated),
+                "4-gram": (bleu_score_4/num_of_rows_calculated),
+                "cumulative-1-gram": (cum_bleu_score_1/num_of_rows_calculated),
+                "cumulative-2-gram": (cum_bleu_score_2/num_of_rows_calculated),
+                "cumulative-3-gram": (cum_bleu_score_3/num_of_rows_calculated),
+                "cumulative-4-gram": (cum_bleu_score_4/num_of_rows_calculated)}
+
+    return results
+
+def count_bleu_score(model_count, real_answers, questions):
+    preds = []
+    for question in questions:
+        outputs = model_count.generate(question)
+        decoded_output = model_count.dec_tokenizer.decode(outputs[0])
+        preds.append(decoded_output)
+    return calculate_bleu(preds, questions, real_answers)
--- FLUENT_REFACTORED_24/main_5pre.py
+++ FLUENT_REFACTORED_24/main_5pre.py
@@ -4,6 +4,8 @@ from torch import nn
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from data import qa_paired, qa_paired_eval
 from architecture_5pre import FLUENTSOTA
+from evaluation_tool import calculate_bleu, count_bleu_score
+
 import torch
 import matplotlib.pyplot as plt
 import time
@@ -53,4 +55,78 @@ for param in enc_model.encoder.layer[:-15].parameters():
 print("Encoder Trainable Parameters : {}%".format(sum(p.numel() for p in enc_model.parameters() if p.requires_grad)/sum(p.numel() for p in enc_model.parameters())*100))
 print("Decoder Trainable Parameters : {}%".format(sum(p.numel() for p in dec_model.parameters() if p.requires_grad)/sum(p.numel() for p in dec_model.parameters())*100))
 
+model = FLUENTSOTA(enc_model, dec_model, enc_tokenizer, dec_tokenizer)
+model.to(device)
 
+all_params = sum(p.numel() for p in model.parameters())
+trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+untrainable_params = all_params - trainable_params
+print(f'All parameters: {all_params:,}')
+print(f'Trainable parameters: {trainable_params:,} ({trainable_params/all_params*100:.2f}%)')
+print(f'Untrainable parameters: {untrainable_params:,} ({untrainable_params/all_params*100:.2f}%)')
+
+bleu_score_eval = pd.DataFrame(columns=['Epoch', '1-gram', '2-gram', '3-gram', '4-gram', 'cumulative-1-gram', 'cumulative-2-gram', 'cumulative-3-gram', 'cumulative-4-gram'])
+bleu_score_train = pd.DataFrame(columns=['Epoch', '1-gram', '2-gram', '3-gram', '4-gram', 'cumulative-1-gram', 'cumulative-2-gram', 'cumulative-3-gram', 'cumulative-4-gram'])
+
+questions = qa_paired_test['Pertanyaan'].apply(lambda x: x.lower().replace('[BOS]', '').replace('[EOS]', '')).to_list()
+answers = qa_paired_test['Jawaban'].apply(lambda x: x.replace('[BOS]', '').replace('[EOS]', '').lower().strip()).to_list()
+questions_eval = qa_paired_eval['Pertanyaan'].apply(lambda x: x.lower().replace('[BOS]', '').replace('[EOS]', '')).to_list()
+answers_eval = qa_paired_eval['Jawaban'].apply(lambda x: x.replace('[BOS]', '').replace('[EOS]', '').lower().strip()).to_list()
+
+epochs = 500
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+criterion = nn.CrossEntropyLoss()
+
+print("start training")
+for ep in range(epochs):
+    torch.cuda.empty_cache()
+
+    total_loss = 0
+    for instance in qa_paired_test.iterrows():
+        # print("---------------")
+        optimizer.zero_grad()
+
+        pertanyaan = instance[1]['Pertanyaan']
+        jawaban = instance[1]['Jawaban']
+        jawaban_withpre = '[PRE1][PRE2][PRE3][PRE4][PRE5]' + jawaban
+
+        tokenized_jawaban_withpre = model.dec_tokenizer(jawaban_withpre)
+        tokenized_jawaban_withpre = torch.tensor(tokenized_jawaban_withpre['input_ids']).unsqueeze(0)
+
+        enc_logits = model.encoding(pertanyaan)
+        output = model.decoding_train(enc_logits, target=jawaban, target_with_pre=tokenized_jawaban_withpre)
+
+        loss = output.loss
+        total_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    print(f'Epoch {ep+1}/{epochs} - Loss: {total_loss:.4f}')
+    if (ep+1) % 10 == 0:
+        print(f'\n-----------------------------------------')
+        test_question = qa_paired_test['Pertanyaan'].iloc[0]
+        outputs = model.generate(test_question)
+        decoded_output = model.dec_tokenizer.decode(outputs[0])
+        print(f'Q >>> {test_question}')
+        print(f'A <<< {decoded_output}')
+        test_question = qa_paired_test['Pertanyaan'].iloc[1]
+        outputs = model.generate(test_question)
+        decoded_output = model.dec_tokenizer.decode(outputs[0])
+        print(f'Q >>> {test_question}')
+        print(f'A <<< {decoded_output}')
+        test_question = qa_paired_test['Pertanyaan'].iloc[4]
+        outputs = model.generate(test_question)
+        decoded_output = model.dec_tokenizer.decode(outputs[0])
+        print(f'Q >>> {test_question}')
+        print(f'A <<< {decoded_output}')
+        print(f'-----------------------------------------\n')
+
+    if (ep+1) % 10 == 0:
+        bleu_result = count_bleu_score(model, answers_eval, questions_eval)
+        bleu_score_eval = pd.concat([bleu_score_eval, pd.DataFrame({'Epoch': ep+1, **bleu_result}, index=[len(bleu_score_eval)])], ignore_index=True)
+        print(f'BLEU Score Eval: {bleu_result["cumulative-4-gram"]:.4f}\n')
+
+        bleu_result = count_bleu_score(model, answers, questions)
+        bleu_score_train = pd.concat([bleu_score_train, pd.DataFrame({'Epoch': ep+1, **bleu_result}, index=[len(bleu_score_train)])], ignore_index=True)
+        print(f'BLEU Score Train: {bleu_result["cumulative-4-gram"]:.4f}\n')
+print("finished training")
