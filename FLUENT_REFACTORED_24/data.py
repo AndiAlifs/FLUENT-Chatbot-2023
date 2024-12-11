ac1787404871a4465b872485f3d720d5e7d743b4
Andi Alifsyah <andyalyfsyah4@gmail.com>
2024-12-11 12:41:05 +0700

    update data

--- FLUENT_REFACTORED_24/data.py
+++ FLUENT_REFACTORED_24/data.py
@@ -16,3 +16,8 @@ print("finished loading data from data.py, get {} qa_paired".format(len(qa_paire
 qa_paired_eval = knowledgebase_eval.drop(columns=knowledgebase_eval.columns.drop(['Pertanyaan', 'Jawaban']))
 qa_paired_eval.dropna(inplace=True)
 print("finished loading data from data.py, get {} qa_paired_eval".format(len(qa_paired_eval)))
+
+qa_paired = qa_paired.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+qa_paired['Jawaban'] = qa_paired['Jawaban'].apply(lambda x: '[BOS]' + x + '[EOS]')
+qa_paired = qa_paired.reset_index(drop=True)
+print("added BOS and EOS token to qa_paired, sample qa_paired: {}".format(qa_paired.head(1)))
