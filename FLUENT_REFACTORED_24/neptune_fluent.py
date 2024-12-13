1a38c93d75bddba523085ff2624ee280b9a0c1c6
Andi Alifsyah <andyalyfsyah4@gmail.com>
2024-12-13 18:08:49 +0700

    fix neptune

--- FLUENT_REFACTORED_24/neptune_fluent.py
+++ FLUENT_REFACTORED_24/neptune_fluent.py
@@ -1,7 +1,7 @@
 import neptune
 
 class Neptune_Fluent:
-    def __init__(self, enc_model, dec_model):
+    def mulai(enc_model, dec_model):
         run = neptune.init_run(
             project="andialifs/fluent-tesis-playground-24",
             api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjZTY2YWQ3My04OTBkLTQ2OWUtYTc1Ni1jYjk0MGZhMWFiNGEifQ==",
