da0b529137e01a59085dfc5ef2fb9afdb37f7e9e
Andi Alifsyah <andyalyfsyah4@gmail.com>
2025-01-22 20:36:44 +0700

    update 3 pre large encoder

---
+++ FLUENT_REFACTORED_24/architecture_3pre_largeenc.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class FLUENTSOTA(nn.Module):
+    def __init__(self, enc_model, dec_model, enc_tokenizer, dec_tokenizer, max_length=200):
+        super(FLUENTSOTA, self).__init__()
+        self.enc_model = enc_model
+        self.dec_model = dec_model
+        self.enc_tokenizer = enc_tokenizer
+        self.dec_tokenizer = dec_tokenizer
+        self.enc_mapper = nn.Linear(1024, 1024)
+        self.enc_mapper2 = nn.Linear(1024, 1024)
+        self.prefix_nn = nn.Linear(1024, 1024)
+        self.prefix_nn2 = nn.Linear(1024, 1024)
+        self.prefix_nn3 = nn.Linear(1024, 1024)
+        self.max_length = max_length
+    
+    def encoding(self, sentence):
+        tokens = self.enc_tokenizer(sentence, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+        tokens = tokens.to(device)
+        with torch.no_grad():
+            output = self.enc_model(**tokens)
+        enc_logits = output.last_hidden_state.sum(dim=1)
+        enc_logits = self.enc_mapper(enc_logits).to(device)
+        enc_logits = self.enc_mapper2(enc_logits).to(device)
+        return enc_logits
+
+    def get_embedding(self, sentence):
+        tokens = self.dec_tokenizer(sentence, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+        tokens = tokens.to(device)
+        wte = self.dec_model.get_input_embeddings()
+        return wte(tokens['input_ids'])
+
+    def dec_tokenizer(self, sentence):
+        tokens = self.dec_tokenizer(sentence, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+        return tokens
+
+    def add_prefix(self, enc_logits):
+        prefix_1 = self.prefix_nn(enc_logits)
+        prefix_2 = self.prefix_nn2(enc_logits)
+        prefix_3 = self.prefix_nn3(enc_logits)
+
+        prefixs = torch.stack((prefix_1, prefix_2, prefix_3), dim=1)
+        return prefixs
+
+    def decoding_train(self, enc_logits, target, target_with_pre):
+        prefixs = self.add_prefix(enc_logits)
+        embed = self.get_embedding(target)
+        pref_with_embed = torch.cat((prefixs, embed), dim=1)
+        output = self.dec_model(inputs_embeds=pref_with_embed, labels=target_with_pre)
+        return output
+
+    def generate(self, quest):
+        enc_logits = self.encoding(quest)
+        prefix_se = '[BOS]'
+        prefix_dec_embed = self.get_embedding(prefix_se)
+        prefixs = self.add_prefix(enc_logits)
+        
+        pref_with_embed = torch.cat((prefixs, prefix_dec_embed), dim=1)
+
+        output = self.dec_model.generate(   inputs_embeds=pref_with_embed, 
+                                            max_length=self.max_length, 
+                                            pad_token_id=self.dec_model.config.eos_token_id)
+        returned_output = []
+        for i in range(len(output[0])):
+            if output[0][i] != self.dec_model.config.eos_token_id:
+                returned_output.append(output[0][i])
+            else:
+                break
+        return torch.tensor(returned_output).unsqueeze(0)
