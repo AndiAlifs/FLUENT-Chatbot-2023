a52a81bb739365d423edb2b17b0a51808293960d
Andi Alifsyah <andyalyfsyah4@gmail.com>
2025-01-22 20:39:07 +0700

    update 3 pre large encoder

--- FLUENT_REFACTORED_24/neptune_fluent.py
+++ FLUENT_REFACTORED_24/neptune_fluent.py
@@ -1,7 +1,7 @@
 import neptune
 
 class Neptune_Fluent:
-    def mulai(enc_model, dec_model):
+    def mulai(enc_model, dec_model, num_pre_token=5):
         run = neptune.init_run(
             project="andialifs/fluent-tesis-playground-24",
             api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjZTY2YWQ3My04OTBkLTQ2OWUtYTc1Ni1jYjk0MGZhMWFiNGEifQ==",
@@ -9,7 +9,8 @@ class Neptune_Fluent:
 
         param = {
             "encoder_model" : enc_model,
-            "decoder_model" : dec_model
+            "decoder_model" : dec_model,
+            "num_pre_token" : num_pre_token
         }
         run["parameters"] = param
 
